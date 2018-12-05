81df0411a8b88fa50f0acf15e1191d764a38c44b
Athena99 <34299068+Rishika-Rathore@users.noreply.github.com>
2018-12-05 17:02:02 +0530

    Update and rename preprocessCaption.py.py to preprocessCaption.py

---
+++ preprocessCaption.py
@@ -0,0 +1,21 @@
+#Preprocessing of Captions and images in a format to feed into the network
+
+import json
+from pprint import pprint
+
+with open('captions_val2014.json') as f:
+    data = json.load(f)
+
+ #Now we will extract 'id' column as id of the image and 'caption ' column to get cation of the image.
+
+
+item=data['annotations']
+#
+ids=[]
+captions=[]
+
+for i in item:
+	ids.append(item['id'])
+	captions.append(item['captions'])
+
+
